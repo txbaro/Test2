987adfdb23ecb388c053a1426d21e575b18552c1
Baro <voduongtonbao2005@gmail.com>
2025-06-06 07:58:13 +0700

    Test2

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.test2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,11 +28,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button btnSubmit = findViewById(R.id.btnSubmit);
+        EditText editName = findViewById(R.id.nameText);
         btnSubmit.setOnClickListener(v -> {
-            int luckyNumber = generateLuckyNumber();
-            Intent intent = new Intent(MainActivity.this, RandomActivity.class);
-            intent.putExtra("lucky_number", luckyNumber);
-            startActivity(intent);
+            String name = editName.getText().toString().trim();
+
+            if (name.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập tên", Toast.LENGTH_SHORT).show();
+            } else {
+                int luckyNumber = generateLuckyNumber();
+                Intent intent = new Intent(MainActivity.this, RandomActivity.class);
+                intent.putExtra("lucky_number", luckyNumber);
+                startActivity(intent);
+            }
         });
     }
 
